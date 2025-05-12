3d4c50be62d45c8a1f68bdf1226af8bd361c16f4
Alexander-Agu <theonlyagu@gmail.com>
2025-05-12 14:55:49 +0200

    Created API entities

---
+++ Employee.Management.API/Entities/Department.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Employee.Management.API.Entities;
+
+public class Department
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+}
---
+++ Employee.Management.API/Entities/Employee.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Employee.Management.API.Entities;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public required int DepartmentId { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Email { get; set; }
+    public required int Phone { get; set; }
+}
