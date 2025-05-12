8f7793ed29fe644b557f70c8e92b307e1a9b56e5
Alexander-Agu <theonlyagu@gmail.com>
2025-05-12 19:02:35 +0200

    Fixing errors

--- Employee.Management.API/Program.cs
+++ Employee.Management.API/Program.cs
@@ -1,11 +1,12 @@
 using Employee.Management.API.Data;
+using Employee.Management.API.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connString = builder.Configuration.GetConnectionString("EmployeeManagement");
 builder.Services.AddSqlite<EmployeeManagementContext>(connString);
 var app = builder.Build();
-
+app.MapEmployeeEnpoints();
 await app.MigrateDbAsync();
 
 
